afe768659a51e51e5f72422a9396c11b59be7146
jotaefecueme <50833571+jotaefecueme@users.noreply.github.com>
2024-06-07 13:29:16 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Global variables
 var serverIP = "http://95.60.72.15";
 var serverPort = "1024";
 
@@ -15,8 +16,10 @@ function consultarServicio() {
         return;
     }
 
+    // Obtener el valor del campo de texto personalizado
     var customInput = document.getElementById("customInput").value;
 
+    // Si se ha ingresado un valor en el campo personalizado, usarlo como "lastSystemTurn"
     if (customInput.trim() !== "") {
         lastSystemTurn = customInput;
     }
@@ -28,6 +31,7 @@ function consultarServicio() {
 
     console.log("Request data:", data);
 
+    // Show loading indicator
     document.getElementById("loading").style.display = "block";
     document.getElementById("debugMessages").innerText = "Sending request to server...";
 
@@ -35,13 +39,15 @@ function consultarServicio() {
     xhr.open("POST", `${serverIP}:${serverPort}/ask`, true);
     xhr.setRequestHeader("Content-Type", "application/json");
 
-    var timeout = 15000; 
-    var timer; 
+    var timeout = 15000; // 15 seconds
+    var timer; // Variable to hold the timer
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
+            // Hide loading indicator
             document.getElementById("loading").style.display = "none";
 
+            // Clear the timer when the response is received
             clearTimeout(timer);
 
             if (xhr.status === 200) {
@@ -53,14 +59,17 @@ function consultarServicio() {
                 var response = JSON.parse(xhr.responseText);
                 console.log("Server response:", response);
 
+                // Show server response in the user interface
                 var responseHTML = "<ul>";
                 for (var key in response) {
                     responseHTML += "<li><strong>" + key + ":</strong> " + JSON.stringify(response[key]) + "</li>";
                 }
                 responseHTML += "</ul>";
 
+                // Add content to the response container
                 document.getElementById("response").innerHTML = responseHTML;
 
+                // Show response time message
                 var responseTimeMessage = "<p>Response time: " + responseTime + " seconds</p>";
                 document.getElementById("response").innerHTML += responseTimeMessage;
             } else {
@@ -73,12 +82,13 @@ function consultarServicio() {
 
     xhr.send(JSON.stringify(data));
 
+    // Set the timeout
     timer = setTimeout(function() {
         console.error("Request timed out.");
         document.getElementById("debugMessages").innerText = "Request timed out. Service is unavailable.";
         document.getElementById("response").innerHTML = "<p>Service is unavailable. Please try again later.</p>";
         document.getElementById("loading").style.display = "none";
-        xhr.abort(); 
+        xhr.abort(); // Abort the request if it takes too long
     }, timeout);
 }
 
